9d693cf496fe77e72486d6cea66ca4555777e965
Andrew Argeros <aargeros15@gmail.com>
2022-04-22 10:50:31 -0500

    Update get_salaries.py

--- get_salaries.py
+++ get_salaries.py
@@ -11,7 +11,7 @@ tqdm.pandas()
 def make_driver():
   chrome_options = uc.ChromeOptions()
   prefs = {"profile.managed_default_content_settings.images": 2,
-          "javascript.enabled": False}
+          'profile.managed_default_content_settings.javascript': 2}
   chrome_options.add_experimental_option("prefs", prefs)
   driver = uc.Chrome(executable_path = ChromeDriverManager().install(),
                       options= chrome_options)
@@ -19,23 +19,25 @@ def make_driver():
   driver.set_page_load_timeout(time_to_wait=10)
   return driver
 
-def get_salary(link): 
+def get_salary(link, driver): 
   driver.get(link) 
   salary = driver.find_element_by_tag_name('table').find_elements_by_tag_name('tr')
   salary = salary[len(salary)-1].find_elements_by_tag_name('td')[1].get_attribute('innerHTML')
   salary = re.sub('[^0-9.]', '', salary)
   return salary
 
-driver = make_driver()
+def main():
+  driver = make_driver()
+  data = pd.read_csv("VA_employees_partial.csv")
+  chunk_list = np.array_split(data, 4)
+  chunk = chunk_list[int(sys.argv[1])-1]
+  
+  print(f"There are {data.shape[0]} Employees in the entire dataset")
+  print(f"There are {chunk.shape[0]} Employees in the chunk {sys.argv[1]}")
 
-data = pd.read_csv("VA_employees_partial.csv")
+  chunk['salary'] = chunk['link'].progress_apply(get_salary, driver=driver)
 
-chunk_list = np.array_split(data, 4)
-chunk = chunk_list[sys.argv[1]-1]
+  chunk.to_csv(f"VA_employees_salaries_chunk{sys.argv[1]}.csv", index=False)
 
-print(f"There are {data.shape[0]} Employees in the entire dataset")
-print(f"There are {chunk.shape[0]} Employees in the chunk {sys.argv[1]}")
-
-chunk['salary'] = chunk['link'].progress_apply(get_salary)
-
-chunk.to_csv(f"VA_employees_salaries_chunk{sys.argv[1]}.csv", index=False)
+if __name__ == "__main__":
+  main()
